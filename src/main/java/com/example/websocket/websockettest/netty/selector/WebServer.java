80da4ac7f80bde05e65dc125518c41d813a6e201
xw <3597906135@qq.com>
2019-08-08 17:05:50 +0800

    netty 之选择器selector

---
+++ src/main/java/com/example/websocket/websockettest/netty/selector/WebServer.java
@@ -0,0 +1,81 @@
+package com.example.websocket.websockettest.netty.selector;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * 测试使用selector 选择器
+ * 选择器键 或 通道注册
+ * 服务端
+ * @author xw
+ * @date 2019/8/2 12:00
+ */
+public class WebServer {
+
+    public static void main(String[] args) {
+        try {
+            // 打开通道
+            ServerSocketChannel ssc = ServerSocketChannel.open();
+            // 为该通道绑定一个端口号
+            ssc.socket().bind(new InetSocketAddress("127.0.0.1", 8000));
+            // 定义为非阻塞
+            ssc.configureBlocking(false);
+
+            // 打开选择器
+            Selector selector = Selector.open();
+            // 注册 channel，并且指定感兴趣的事件是 Accept
+            ssc.register(selector, SelectionKey.OP_ACCEPT);
+
+            ByteBuffer readBuff = ByteBuffer.allocate(1024);
+            ByteBuffer writeBuff = ByteBuffer.allocate(128);
+            writeBuff.put("received".getBytes());
+            writeBuff.flip();
+
+            while (true) {
+                int nReady = selector.select();
+                // 获取选择键
+                Set<SelectionKey> keys = selector.selectedKeys();
+                Iterator<SelectionKey> it = keys.iterator();
+
+                while (it.hasNext()) {
+                    SelectionKey key = it.next();
+                    it.remove();
+
+                    if (key.isAcceptable()) {
+                        // 创建新的连接，并且把连接注册到selector上，而且，
+                        // 声明这个channel只对读操作感兴趣。
+                        SocketChannel socketChannel = ssc.accept();
+                        socketChannel.configureBlocking(false);
+                        socketChannel.register(selector, SelectionKey.OP_READ);
+                    }
+                    else if (key.isReadable()) {
+                        SocketChannel socketChannel = (SocketChannel) key.channel();
+                        readBuff.clear();
+                        socketChannel.read(readBuff);
+
+                        readBuff.flip();
+                        System.out.println("received : " + new String(readBuff.array()));
+                        key.interestOps(SelectionKey.OP_WRITE);
+                    }
+                    else if (key.isWritable()) {
+                        writeBuff.rewind();
+                        SocketChannel socketChannel = (SocketChannel) key.channel();
+                        socketChannel.write(writeBuff);
+                        key.interestOps(SelectionKey.OP_READ);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
